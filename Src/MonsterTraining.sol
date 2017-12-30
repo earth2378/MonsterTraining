f1ad39277f075c014a294e3c4395f025f626c0f7
earth2378 <earth2378@hotmail.com>
2017-12-30 23:15:38 +0700

    edit

--- Src/MonsterTraining.sol
+++ Src/MonsterTraining.sol
@@ -9,28 +9,37 @@ contract MonsterTraining{
     mapping(address => Inventory)inventory;
     mapping(address => bytes32)passwordStorage;
     address GM;
-    
-    function MonsterTraining(string password)public{
+
+    function MonsterTraining()public{
         GM = msg.sender;
-        register(password);
     }
-    
+
     function getBalance()public view returns(int){
         inventory[msg.sender].getZil();
     }
-    
+    //function selectedMonster()public view returns()
+
     function a()public view returns(bytes32){
         return passwordStorage[msg.sender];
     }
-    
+
     function register(string password)public{
         require(passwordStorage[msg.sender] == bytes32(0));
         passwordStorage[msg.sender] = keccak256(msg.sender,password);
         realm[msg.sender] = new Monsters();
         inventory[msg.sender] = new Inventory();
     }
-    
+
     function topUp() public payable{
         inventory[msg.sender].topUp();
+    }
+
+    function hatchMonster(string name)public{
+        inventory[msg.sender].useTicket();
+        realm[msg.sender].hatch(name);
+    }
+    
+    function loadMonster(string name)public{
+        realm[msg.sender].load(name);
     }
 }
